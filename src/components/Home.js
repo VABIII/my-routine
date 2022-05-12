62e93d1d0c8c369e62113f317b7a656b72594cbf
VABIII <tbengtson82@gmail.com>
2022-05-11 23:11:17 -0500

    Added image and some style to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Home.css'
 
 const Home = () => {
     return (
@@ -6,6 +7,9 @@ const Home = () => {
             <div>
                 <h1>My Routine</h1>
             </div>
+            <img
+                src="https://images.unsplash.com/photo-1574680096145-d05b474e2155?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                alt="female with squat bar on shoulders"/>
         </div>
     );
 };
